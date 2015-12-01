97c98d08f7ca0776f916663ae3976c8af49e42a6
Yohan Seimon <yohanseimon@yahoo.com>
2015-12-01 12:02:16 +1100

    Added Library and Unit Tests projects

---
+++ Matasano.Library.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matasano.Library.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
